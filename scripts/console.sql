4df6e387ad156f54865109199097e85d218050af
João Antão <jp.antao@campus.fct.unl.pt>
2021-06-22 00:32:14 +0100

    added sql files

--- scripts/console.sql
+++ scripts/console.sql
@@ -1,32 +1,22 @@
--- sequence creation
 CREATE SEQUENCE student_number_generator;
 SELECT * from student_number_generator;
 
---------------------------------------------------------------------
+select nextval('student_number_generator');
 
--- auto incrementing column
 CREATE TABLE students (
-    number INT NOT NULL DEFAULT NEXTVAL('student_number_generator'), 
+    number INT NOT NULL DEFAULT NEXTVAL('student_number_generator'),
     name VARCHAR(90)
 );
 
--- Or if we dont want to use the student_number_generator sequence:
-CREATE TABLE students (
-    number SERIAL, 
-    name VARCHAR(90)
-);
-
---------------------------------------------------------------------
-
--- database for the example
-
-DROP TABLE  students;
 CREATE TABLE students (
     number SERIAL,
     name VARCHAR(90)
 );
 
+INSERT INTO students (name) VALUES ('Bob');
+INSERT INTO students (name) VALUES ('Alice');
+
 SELECT * FROM students;
 
-DROP TABLE  students;
+DROP TABLE students;
 DROP SEQUENCE student_number_generator;
