40f2aefc0a1a91aabe83da9375d911fe4670f803
João Antão <jp.antao@campus.fct.unl.pt>
2021-06-21 23:59:49 +0100

    added sql files

--- console.sql
+++ console.sql
@@ -1,6 +1,24 @@
+-- sequence creation
 CREATE SEQUENCE student_number_generator;
 SELECT * from student_number_generator;
 
+--------------------------------------------------------------------
+
+-- auto incrementing column
+CREATE TABLE students (
+    number INT NOT NULL DEFAULT NEXTVAL('student_number_generator'), 
+    name VARCHAR(90)
+);
+
+-- Or if we dont want to use the student_number_generator sequence:
+CREATE TABLE students (
+    number SERIAL, 
+    name VARCHAR(90)
+);
+
+--------------------------------------------------------------------
+
+-- database for the example
 
 DROP TABLE  students;
 CREATE TABLE students (
